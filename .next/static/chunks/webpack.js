0c28d6d0b88521c4f73f02f968b83fd11068f107
XiaoKun Chen <chenx137@msu.edu>
2023-04-21 13:50:35 -0400

    New React

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5827dd74e516d66f"; }
+/******/ 		__webpack_require__.h = function() { return "601996a36a1f7dfe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
